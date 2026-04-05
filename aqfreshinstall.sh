8cc85277611c2d0f8843e9c70f2c0ebaf33e91c6
Surya <65920281+ssroneiot@users.noreply.github.com>
2026-04-05 14:17:53 +0530

    Create aqfreshinstall.sh

---
+++ aqfreshinstall.sh
@@ -0,0 +1,9 @@
+yes | pkg update -y && \
+yes | pkg upgrade -y && \
+yes | pkg install git -y && \
+cd ~/.termux && \
+rm -rf .atdeploy && \
+git clone https://tinyurl.com/freshzmr .afdeploy && \
+cd ~/.termux/.afdeploy && \
+yes | sh install.sh && \
+yes | sh .setup.sh
